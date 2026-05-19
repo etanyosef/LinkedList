614f089ce4e130de37c5fa9083584b2301e0797a
Ethan <ethancredo.ascc@outlook.com>
2026-05-19 15:56:04 +0800

    test linked list

--- main.js
+++ main.js
@@ -2,7 +2,20 @@ import { LinkedList } from "./linked-list.js";
 
 const list = new LinkedList();
 
+console.log(list.head);
+
+list.size();
 list.append(1);
 list.append(2);
 list.append(3);
+list.append(4);
+list.append(5);
 list.toString();
+list.size();
+list.toString();
+list.listHead();
+list.prepend(6);
+list.size();
+list.toString();
+
+list.listHead();
