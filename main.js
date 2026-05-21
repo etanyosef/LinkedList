d959407bc9adab17fcc6ea848fbf6226e08dfb6d
Ethan <ethancredo.ascc@outlook.com>
2026-05-21 12:04:01 +0800

    testing linkedlist

--- main.js
+++ main.js
@@ -11,29 +11,31 @@ list.append(3);
 list.append(4);
 list.append(5);
 list.toString();
-list.size();
-list.toString();
-list.getHead();
-list.prepend(6);
-list.size();
-list.toString();
 
-list.getHead();
-list.tail();
-list.append(7);
-list.toString();
-list.tail();
-list.at(7);
+// list.size();
+// list.toString();
+// list.getHead();
+// list.prepend(6);
+// list.size();
+// list.toString();
 
-console.log(list.contains(6));
-list.findIndex(3);
-list.pop();
-list.toString();
-console.log(list.contains(6));
-list.findIndex(3);
-list.pop();
-list.toString();
+// list.getHead();
+// list.tail();
+// list.append(7);
+// list.toString();
+// list.tail();
+// list.at(7);
 
-list.findIndex(8);
+// console.log(list.contains(6));
+// list.findIndex(3);
+// list.pop();
+// list.toString();
+// console.log(list.contains(6));
+// list.findIndex(3);
+// list.pop();
+// list.toString();
 
-list.insertAt(0);
+// list.findIndex(8);
+
+list.insertAt(3, 6, 7);
+list.toString();
